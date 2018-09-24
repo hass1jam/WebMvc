a5ba326e25bce83a16a9e221d171f409ea7b0982
Hassanoor Jamman-Contractor <hjamman@toryburch.com>
2018-09-24 16:01:39 -0400

    Add project files.

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Title: Contact/Index";
+            return View();
+        }
+    }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((host, cfg )=>
+            {
+                var path1 = Path.Combine(host.HostingEnvironment.ContentRootPath, "config.json");
+                var path2 = Path.Combine(host.HostingEnvironment.ContentRootPath, "appsettings.json");
+                cfg.AddJsonFile(path2, false, true).AddJsonFile(path1, false, true);
+            })
+            .UseStartup<Startup>();
+    }
+}
